2ff4eb2813ddb4590b08f4786910d1884a6cb376
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-16 12:00:34 +0600

    get the email address inside the email  input field

--- js/login.js
+++ js/login.js
@@ -1,5 +1,16 @@
 // step-01 add even handler with submit button  
 document.getElementById('btn-submit').addEventListener('click', function () {
     // console.log('okkkkkkkkkk');
+
+
+
+    // step - 02 get the email address inside the email  input field
+    // allways remember to use . value to get text form an input field 
+    const EmailField = document.getElementById('user-email');
+    const email = EmailField.value;
+    console.log(email);
+
+
+
 })
 
